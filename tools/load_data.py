631f829ce26cc089c22eacb9007fd2cab7bcc060
florent <flobarre.com@gmail.com>
2023-10-13 17:18:34 +0200

    (load_data.py): refacto

--- tools/load_data.py
+++ tools/load_data.py
@@ -13,6 +13,7 @@ def load_data():
     for data_all_path in list_data_all_path:
         file_name = data_all_path.split('\\')[-1]
         file_name_without_extension = file_name.split('.')[0]
+        file_name_without_directory = file_name_without_extension.split('/')[-1]
         df = pd.read_csv(data_all_path)
-        data_raw_dict[f'df_{file_name_without_extension}'] = df
+        data_raw_dict[f'df_{file_name_without_directory}'] = df
     return data_raw_dict
